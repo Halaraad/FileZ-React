2febee172058600df23fa53a53c7a8c26a562b63
rasheed92 <emperor.rasheed@gmail.com>
2019-01-13 01:10:22 +0300

    fix

--- src/component/signin.js
+++ src/component/signin.js
@@ -5,64 +5,64 @@ import { NavLink } from "react-router-dom";
 const SignIn = () => {
   return (
     <Context.Consumer>
-      {ctx => {
-        return (
+    {ctx => {
+      return (
 
-            <React.Fragment>
-              <div className="mainSignIn">
-                <div>
-                  <div className="logoSignin" />
-                  <div className="namelogo">Filez</div>
-                  <img
-                    className="line"
-                    src={require("../assets/line.png")}
-                    alt=""
-                  />
-                  <div className="desclogo">Keep your files online.</div>
-                  <img
-                    className="info"
-                    src={require("../assets/info.png")}
-                    alt=""
-                  />
-                  <img
-                    className="msg"
-                    src={require("../assets/msg.png")}
-                    alt=""
-                  />
-                  <img
-                    className="linev"
-                    src={require("../assets/Linev.png")}
-                    alt=""
-                  />
-                </div>
-                <div className="auth">
-                  <div className="Authenticate">Authenticate</div>
+          <React.Fragment>
+            <div className="mainSignIn">
+              <div>
+                <div className="logoSignin" />
+                <div className="namelogo">Filez</div>
+                <img
+                  className="line"
+                  src={require("../assets/line.png")}
+                  alt=""
+                />
+                <div className="desclogo">Keep your files online.</div>
+                <img
+                  className="info"
+                  src={require("../assets/info.png")}
+                  alt=""
+                />
+                <img
+                  className="msg"
+                  src={require("../assets/msg.png")}
+                  alt=""
+                />
+                <img
+                  className="linev"
+                  src={require("../assets/Linev.png")}
+                  alt=""
+                />
+              </div>
+              <div className="auth">
+                <div className="Authenticate">Authenticate</div>
 
-                  <form>
-                    <div className="username">
-                      <i class="material-icons">account_circle</i>
-                      <input type="text" placeholder="User Name" />
-                    </div>
-                    <div className="password">
-                      <i class="material-icons">lock</i>
-                      <input type="text" placeholder="Password" />
-                    </div>
-                    <button className="btnauth">Authenticate</button>
-                  </form>
-
-                  <div className="linkSignUp">
-                    <span>Don’t have an account? </span>
-
-                      <a href="/signup"> Sign Up</a>
-
+                <form>
+                  <div className="username">
+                    <i class="material-icons">account_circle</i>
+                    <input type="text" placeholder="User Name" />
                   </div>
+                  <div className="password">
+                    <i class="material-icons">lock</i>
+                    <input type="text" placeholder="Password" />
+                  </div>
+                  <button className="btnauth">Authenticate</button>
+                </form>
+
+                <div className="linkSignUp">
+                  <span>Don’t have an account? </span>
+                  <NavLink exact to="/signup">
+                    <a href="/signup"> Sign Up</a>
+                  </NavLink>
                 </div>
               </div>
-            </React.Fragment>
+            </div>
+          </React.Fragment>
 
-        );
-      }}
-    </Context.Consumer>
+      );
+    }}
+  </Context.Consumer>
   );
 };
 
