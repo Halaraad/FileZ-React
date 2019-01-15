98808771198f590ee30b611e2be39920e31da311
rasheed92 <emperor.rasheed@gmail.com>
2019-01-15 19:36:07 +0300

    if user already login login and singup will redirect him to user home page

--- src/component/signin.js
+++ src/component/signin.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { toaster } from 'evergreen-ui';
+import { toaster,Pane } from 'evergreen-ui';
 import axios from  'axios';
 import Cookies from 'universal-cookie';
 import Context from "./context.js";
+import FilesPage from "./myFile";
+import Loading from '../assets/images/loading.gif';
 
 const cookies = new Cookies();
 
@@ -47,68 +49,83 @@ class SignIn extends React.Component {
   return (
     <Context.Consumer>
       {ctx => {
-        return (
-          <div className="authmain">
-          <NavLink activeClassName="NavLink-auth" exact to ="/signin">
-          <div className="authDiv">
-            <React.Fragment >
-              <div className="mainSignIn">
-                <div className="leftside">
-                <div className="logoSignin"><img width="350" src="/assets/images/home.png" /></div>
-                  <div className="namelogo">Filez</div>
-                  <img
-                    className="line"
-                    src="/assets/images/line.png"
-                    alt=""
-                  />
-                  <div className="desclogo">Keep your files online.</div>
-                  <div className="icons-container">
-                    {/* <img
-                      className="info"
-                      src="/assets/images/facebook.png"
-                      alt="facebook"
-                    />
+        if (ctx.value.isLogin=='login') {
+          return (
+            <FilesPage/>
+            );
+       
+        } else if (ctx.value.isLogin=='notLogin') {
+          return (
+            <div className="authmain">
+            <NavLink activeClassName="NavLink-auth" exact to ="/signin">
+            <div className="authDiv">
+              <React.Fragment >
+                <div className="mainSignIn">
+                  <div className="leftside">
+                  <div className="logoSignin"><img width="350" src="/assets/images/home.png" /></div>
+                    <div className="namelogo">Filez</div>
                     <img
-                      className="msg"
-                      src="/assets/images/email.png"
-                      alt="email"
-                    /> */}
+                      className="line"
+                      src="/assets/images/line.png"
+                      alt=""
+                    />
+                    <div className="desclogo">Keep your files online.</div>
+                    <div className="icons-container">
+                      {/* <img
+                        className="info"
+                        src="/assets/images/facebook.png"
+                        alt="facebook"
+                      />
+                      <img
+                        className="msg"
+                        src="/assets/images/email.png"
+                        alt="email"
+                      /> */}
+                    </div>
+                  </div>
+                  <div className="auth">
+                    <div className="Authenticate">Authenticate</div>
+  
+                    <form className="authform"> 
+                      <div className="email">
+                        <input type="email" placeholder="Email" name="Email"
+                          onChange={this.handleChange.bind(this)}
+                          value={this.state.Email}
+                          />
+                      </div>
+                      <div className="password">
+                        <input type="password" placeholder="Password" name="Password"
+                          onChange={this.handleChange.bind(this)}
+                          value={this.state.Password}
+                          />
+                      </div>
+                      <button className="btnauth"
+                      onClick={this.Login.bind(this)}
+                      >Authenticate</button>
+                    </form>
+  
+                    <div className="linkSignUp">
+                      <span>Don’t have an account? &nbsp;</span>
+                      <NavLink exact to="/signup">
+                        <a href="/signup"> Sign Up</a>
+                      </NavLink>
+                    </div>
                   </div>
                 </div>
-                <div className="auth">
-                  <div className="Authenticate">Authenticate</div>
-
-                  <form className="authform"> 
-                    <div className="email">
-                      <input type="email" placeholder="Email" name="Email"
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.Email}
-                        />
-                    </div>
-                    <div className="password">
-                      <input type="password" placeholder="Password" name="Password"
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.Password}
-                        />
-                    </div>
-                    <button className="btnauth"
-                    onClick={this.Login.bind(this)}
-                    >Authenticate</button>
-                  </form>
-
-                  <div className="linkSignUp">
-                    <span>Don’t have an account? &nbsp;</span>
-                    <NavLink exact to="/signup">
-                      <a href="/signup"> Sign Up</a>
-                    </NavLink>
-                  </div>
-                </div>
+              </React.Fragment>
               </div>
-            </React.Fragment>
+            </NavLink>
             </div>
-          </NavLink>
-          </div>
-        );
+          );
+        }else{
+          return (
+            <Pane id="DivSpinner" display="flex" alignItems="center" justifyContent="center" >
+             <img src={Loading}/>
+             
+            </Pane>
+             )
+        }
+
       }}
     </Context.Consumer>
   )
--- src/component/signup.js
+++ src/component/signup.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { toaster } from 'evergreen-ui';
+import { toaster ,Pane} from 'evergreen-ui';
 import axios from  'axios';
 import Cookies from 'universal-cookie';
 import Context from "./context.js";
-
+import FilesPage from "./myFile";
+import Loading from '../assets/images/loading.gif';
 const cookies = new Cookies();
 
 class SignUp extends React.Component {
@@ -56,75 +57,89 @@ class SignUp extends React.Component {
   return (
     <Context.Consumer>
       {ctx => {
-        return (
-          <div className="authmain">
-          <NavLink activeClassName="NavLink-auth" exact to ="/signup">
-             <div className="authDiv">
-            <React.Fragment>
-         
-              <div className="mainSignIn">
-                <div className="leftside">
-                  <div className="logoSignin"><img width="350" src="/assets/images/home.png" /></div>
-                      <div className="namelogo">Filez</div>
-                      <img
-                        className="line"
-                        src="/assets/images/line.png"
-                        alt=""
-                      />
-                      <div className="desclogo">Keep your files online.</div>
-                      <div className="icons-container">
-                        {/* <img
-                          className="info"
-                          src="/assets/images/facebook.png"
-                          alt="facebook"
-                        />
-                        <img
-                          className="msg"
-                          src="/assets/images/email.png"
-                          alt="email"
-                        /> */}
+                if (ctx.value.isLogin=='login') {
+                  return (
+                    <FilesPage/>
+                    );
+                } else if (ctx.value.isLogin=='notLogin') {
+                  return (
+                    <div className="authmain">
+                    <NavLink activeClassName="NavLink-auth" exact to ="/signup">
+                       <div className="authDiv">
+                      <React.Fragment>
+                   
+                        <div className="mainSignIn">
+                          <div className="leftside">
+                            <div className="logoSignin"><img width="350" src="/assets/images/home.png" /></div>
+                                <div className="namelogo">Filez</div>
+                                <img
+                                  className="line"
+                                  src="/assets/images/line.png"
+                                  alt=""
+                                />
+                                <div className="desclogo">Keep your files online.</div>
+                                <div className="icons-container">
+                                  {/* <img
+                                    className="info"
+                                    src="/assets/images/facebook.png"
+                                    alt="facebook"
+                                  />
+                                  <img
+                                    className="msg"
+                                    src="/assets/images/email.png"
+                                    alt="email"
+                                  /> */}
+                                </div>
+                          </div>
+                              <div className="auth">
+                                <div className="Authenticate">Create Account</div>
+          
+                                <form className="authform">
+                                  <div className="username">
+                                    <input type="text" placeholder="User Name" name="Name"
+                                      onChange={this.handleChange.bind(this)}
+                                      value={this.state.Name}
+                                      />
+                                  </div>
+                                  <div className="email">
+                                    <input type="text" placeholder="Email" name="Email"
+                                      onChange={this.handleChange.bind(this)}
+                                      value={this.state.Email}
+                                      />
+                                  </div>
+                                  <div className="password">
+                                    <input type="password" placeholder="Password" name="Password"
+                                    onChange={this.handleChange.bind(this)}
+                                    value={this.state.Password}
+                                    />
+                                  </div>
+                                  <button className="btnauth" 
+                                  onClick={this.Register.bind(this)}
+                                  >Register</button>
+                                </form>
+                                
+                                <div className="linkSignUp on-signup">
+                                  <span>Already a member? &nbsp;</span>
+                                  <NavLink exact to="/signin">
+                                    <a href="/signin"> Sign In</a>
+                                  </NavLink>
+                                </div>
+                            </div>
+                        </div>
+                      </React.Fragment>
                       </div>
-                </div>
-                    <div className="auth">
-                      <div className="Authenticate">Create Account</div>
+                    </NavLink>
+                    </div>
+                  );
+                }else{
+                  return (
+                    <Pane id="DivSpinner" display="flex" alignItems="center" justifyContent="center" >
+                     <img src={Loading}/>
+                     
+                    </Pane>
+                     )
+                }
 
-                      <form className="authform">
-                        <div className="username">
-                          <input type="text" placeholder="User Name" name="Name"
-                            onChange={this.handleChange.bind(this)}
-                            value={this.state.Name}
-                            />
-                        </div>
-                        <div className="email">
-                          <input type="text" placeholder="Email" name="Email"
-                            onChange={this.handleChange.bind(this)}
-                            value={this.state.Email}
-                            />
-                        </div>
-                        <div className="password">
-                          <input type="password" placeholder="Password" name="Password"
-                          onChange={this.handleChange.bind(this)}
-                          value={this.state.Password}
-                          />
-                        </div>
-                        <button className="btnauth" 
-                        onClick={this.Register.bind(this)}
-                        >Register</button>
-                      </form>
-                      
-                      <div className="linkSignUp on-signup">
-                        <span>Already a member? &nbsp;</span>
-                        <NavLink exact to="/signin">
-                          <a href="/signin"> Sign In</a>
-                        </NavLink>
-                      </div>
-                  </div>
-              </div>
-            </React.Fragment>
-            </div>
-          </NavLink>
-          </div>
-        );
       }}
     </Context.Consumer>
   );
